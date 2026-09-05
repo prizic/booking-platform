f5aa4c3f64c44dad679cfe54278427fc09e65cc7
SEIFSEIF4 <seifsaif151@gmail.com>
2026-09-05 17:58:31 +0300

    fix(db): preserve generated default schema helper

--- packages/supabase-client/src/database.types.ts
+++ packages/supabase-client/src/database.types.ts
@@ -65,7 +65,7 @@ export type Database = {
 
 type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">;
 
-type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "api_v1">];
+type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">];
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
